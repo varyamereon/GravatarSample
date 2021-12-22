d33600139ea86bad40cbff09527be288d391cade
Will Beal <willbeal@outlook.com>
2021-12-22 21:26:45 +0100

    Add project files.

---
+++ GravatarSample/GravatarSample/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GravatarSample;
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
